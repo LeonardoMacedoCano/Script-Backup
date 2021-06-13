773a638c40ad3cfbe4ccbbc186c758df5c955836
LCano <leonardomacedocano@gmail.com>
2021-06-13 02:20:58 -0300

    add backup.bat

---
+++ backup.bat
@@ -0,0 +1,14 @@
+cls
+echo off
+cls
+set ORIGEM = ""
+set DESTINO = ""
+set LOG = ""
+echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+echo #       COPIANDO DE ARQUIVOS E/OU DIRETORIOS        #
+echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+xcopy %ORIGEM% %DESTINO% /E /Y /C /H
+echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+echo # GERANDO LOG DE ARQUIVOS E/OU DIRETORIOS COPIADOS  #
+echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+dir /s %DESTINO% > %LOG%\Log.txt
