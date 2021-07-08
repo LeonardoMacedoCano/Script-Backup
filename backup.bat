821876e60a050defaffb56ba2e37efedd32cbdbb
LCano <leonardomacedocano@gmail.com>
2021-07-07 21:57:18 -0300

    corrigir variaveis

--- backup.bat
+++ backup.bat
@@ -1,14 +1,19 @@
 cls
 echo off
 cls
-set ORIGEM = ""
-set DESTINO = ""
-set LOG = ""
+
+set ORIGEM=""
+set DESTINO=""
+set LOG=""
+
 echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
 echo #       COPIANDO DE ARQUIVOS E/OU DIRETORIOS        #
 echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+
 xcopy %ORIGEM% %DESTINO% /E /Y /C /H
+
 echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
 echo # GERANDO LOG DE ARQUIVOS E/OU DIRETORIOS COPIADOS  #
 echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+
 dir /s %DESTINO% > %LOG%\Log.txt
